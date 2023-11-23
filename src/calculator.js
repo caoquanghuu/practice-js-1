5a78b1ca3e5da76ed3f1a327dbbf814c1b289f6a
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2023-11-23 17:00:53 +0700

    add: create input before calculator when press button

--- src/calculator.js
+++ src/calculator.js
@@ -1,3 +1,73 @@
-if (module.hot) {
-    module.hot.accept();
+window.one = function one() {
+  var input = document.getElementById("result");
+  var a = "1";
+  input.value = input.value + a;
+};
+window.two = function two() {
+  var input = document.getElementById("result");
+  var a = "2";
+  input.value = input.value + a;
+};
+window.three = function three() {
+  var input = document.getElementById("result");
+  var a = "3";
+  input.value = input.value + a;
+};
+window.four = function four() {
+  var input = document.getElementById("result");
+  var a = "4";
+  input.value = input.value + a;
+};
+window.five = function five() {
+  var input = document.getElementById("result");
+  var a = "5";
+  input.value = input.value + a;
+};
+window.six = function six() {
+  var input = document.getElementById("result");
+  var a = "6";
+  input.value = input.value + a;
+};
+window.seven = function seven() {
+  var input = document.getElementById("result");
+  var a = "7";
+  input.value = input.value + a;
+};
+window.eight = function eight() {
+  var input = document.getElementById("result");
+  var a = "8";
+  input.value = input.value + a;
+};
+window.nine = function nine() {
+  var input = document.getElementById("result");
+  var a = "9";
+  input.value = input.value + a;
+};
+window.zero = function zero() {
+  var input = document.getElementById("result");
+  var a = "0";
+  input.value = input.value + a;
+};
+// create dot and check dot already exist or not
+window.dot = function dot() {
+  var input = document.getElementById("result");
+  var ar = input.value.split("");
+  var a = ".";
+  for (var i = 0; i < ar.length; i++) {
+    if ((ar[i] == a)) {
+      break;
+    } else {
+      if ((i == (ar.length - 1) & (ar[i] != a))) {
+        input.value = input.value + a;
+      }
+    }
   }
+};
+window.ce = function ce() {
+  var ce = document.getElementById("result");
+  ce.value = "";
+};
+
+if (module.hot) {
+  module.hot.accept();
+}
