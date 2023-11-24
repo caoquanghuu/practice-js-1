8b7b513dfa124952a895d4249b0920212b6ed8d9
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2023-11-24 15:39:41 +0700

    add: make calculator page work

--- src/calculator.js
+++ src/calculator.js
@@ -1,76 +1,56 @@
-window.one = function one() {
+// get input from numb button
+window.getInput = function getInput(a) {
   var input = document.getElementById("result");
-  var a = "1";
   input.value = input.value + a;
 };
-window.two = function two() {
-  var input = document.getElementById("result");
-  var a = "2";
-  input.value = input.value + a;
-};
-window.three = function three() {
-  var input = document.getElementById("result");
-  var a = "3";
-  input.value = input.value + a;
-};
-window.four = function four() {
-  var input = document.getElementById("result");
-  var a = "4";
-  input.value = input.value + a;
-};
-window.five = function five() {
-  var input = document.getElementById("result");
-  var a = "5";
-  input.value = input.value + a;
-};
-window.six = function six() {
-  var input = document.getElementById("result");
-  var a = "6";
-  input.value = input.value + a;
-};
-window.seven = function seven() {
-  var input = document.getElementById("result");
-  var a = "7";
-  input.value = input.value + a;
-};
-window.eight = function eight() {
-  var input = document.getElementById("result");
-  var a = "8";
-  input.value = input.value + a;
-};
-window.nine = function nine() {
-  var input = document.getElementById("result");
-  var a = "9";
-  input.value = input.value + a;
-};
-window.zero = function zero() {
-  var input = document.getElementById("result");
-  var a = "0";
-  input.value = input.value + a;
-};
+
 // create dot and check dot already exist or not
 window.dot = function dot() {
   var input = document.getElementById("result");
   var ar = input.value.split("");
   var a = ".";
   for (var i = 0; i < ar.length; i++) {
-    if ((ar[i] == a)) {
+    if (ar[i] == a) {
       break;
     } else {
-      if ((i == (ar.length - 1) & (ar[i] != a))) {
+      if ((i == ar.length - 1) & (ar[i] != a)) {
         input.value = input.value + a;
       }
     }
   }
 };
-
-window.multi = function multi() {
+//get calculation and return result
+window.calculation = function calculation(cal) {
   var input = document.getElementById("result");
-  const valueIn1 = parseFloat(input.value);
-  
-}.then( window.ce()).then( function getInput2 () {
+  const valueIn1 = input.value;
+  console.log("input 1", valueIn1);
+  input.value = "";
+  window.displayOutput = function displayOutput() {
+    var input = document.getElementById("result");
+    let valueIn2 = input.value;
+    input.value = "";
+    console.log("input 2", valueIn2);
+    let result;
+    switch (cal) {
+      case "*":
+        result = valueIn1 * valueIn2;
+        break;
+      case "-":
+        result = valueIn1 - valueIn2;
+        break;
+      case "+":
+        result = valueIn1 + valueIn2;
+        break;
+      case "/":
+        result = valueIn1 / valueIn2;
+        break;
+    }
+    input.value = result;
+    console.log("result", result);
+    result = "";
+  };
+};
 
-})
 // function to clear input
 window.ce = function ce() {
   var ce = document.getElementById("result");
