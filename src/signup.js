95c5bd38fc3df3d636f1a80e6d594d19c555180d
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2023-11-28 18:06:58 +0700

    fix: signup page and js code

--- src/signup.js
+++ src/signup.js
@@ -1,99 +1,83 @@
-import axios from "axios";
+const signupBtn = document.getElementById('signup-button');
+const inputName = document.getElementById('input-name');
+const inputEmail = document.getElementById('input-email');
+const inputPhone = document.getElementById('input-phone');
+const inputPassword = document.getElementById('input-password');
+const loadingIcon = document.querySelector('.fa');
+const showBtn = document.getElementById('eye-signup');
+const showError = document.getElementById('exist-error');
 
-var sgp = document.querySelector("#signup-button");
-var uN = document.querySelector("#userName");
-var uE = document.querySelector("#userEmail");
-var showSNP = document.querySelector("#eye-signup");
-var PhoneNumb = document.getElementById("userPhoneN");
-var password = document.getElementById("userPSW");
-var loading = document.querySelector(".fa");
+const axios = require('axios').default;
 
-function Signup() {
-  var userName = uN.value;
-  var userEmail = uE.value;
-  var userPhoneN = PhoneNumb.value;
-  var userPSW = password.value;
-  uN.disabled = true;
-  uE.disabled = true;
-  PhoneNumb.disabled = true;
-  password.disabled = true;
-  setTimeout(() => {
-    if (
-      userName == "" ||
-      userEmail == "" ||
-      userPhoneN == "" ||
-      userPSW == ""
-    ) {
-      alert("please insert all information!!!");
-    } else {
-      axios.get("http://localhost:3000/user").then(function (data) {
-        console.log(data);
-        var length = data.data.length;
-        var checkUser;
-        for (var i = 0; i < length - 1; i++) {
-          if (
-            userName == data.data[i].name ||
-            userEmail == data.data[i].email
-          ) {
-            checkUser = false;
-            var error = document.getElementById("exist-error");
-            error.innerHTML = "user / email already exist";
-            break;
-          }
-        }
-        if (checkUser != false) {
-          axios
-            .post("http://localhost:3000/user", {
-              name: userName,
-              email: userEmail,
-              userPSW: userPSW,
-              PhoneNumb: userPhoneN,
-            })
-            .then(function (response) {
-              console.log(response);
-            })
-            .catch(function (error) {
-              console.log(error);
-            });
-        }
+function sleep(ms) {
+  // eslint-disable-next-line no-promise-executor-return
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+async function Signup() {
+  // Set variable inside
+  const userName = inputName.value;
+  const userEmail = inputEmail.value;
+  const userPhone = inputPhone.value;
+  const userPassword = inputPassword.value;
+  // Disable input field.
+  inputName.disabled = true;
+  inputEmail.disabled = true;
+  inputPhone.disabled = true;
+  inputPassword.disabled = true;
+  // Start loading button.
+  loadingIcon.classList.add('fa-circle-o-notch');
+  loadingIcon.classList.add('fa-spin');
+
+  if (!userName || !userEmail || !userPhone || !userPassword) {
+    showError.innerHTML = 'Please insert all information!!!';
+  } else {
+    const response = await axios.get('http://localhost:3000/user');
+    await sleep(3000);
+    let checkUser;
+    for (let i = 0; i < response.data.length - 1; i += 1) {
+      if (userName === response.data[i].name || userEmail === response.data[i].email) {
+        checkUser = false;
+        showError.innerHTML = 'user / email already exist';
+      }
+    }
+    if (checkUser !== false) {
+      axios.post('http://localhost:3000/user', {
+        name: userName,
+        email: userEmail,
+        userPSW: userPassword,
+        PhoneNumb: userPhone,
       });
     }
-    uN.disabled = false;
-    uE.disabled = false;
-    PhoneNumb.disabled = false;
-    password.disabled = false;
-  }, "3000");
+  }
+  // Enable input field.
+  inputName.disabled = false;
+  inputEmail.disabled = false;
+  inputPhone.disabled = false;
+  inputPassword.disabled = false;
+  // Stop loading button.
+  loadingIcon.classList.remove('fa-circle-o-notch');
+  loadingIcon.classList.remove('fa-spin');
 }
-
+// clear error.
 function clearError() {
-  document.getElementById("exist-error").innerHTML = "";
+  document.getElementById('exist-error').innerHTML = '';
 }
 
 function showPSW() {
-  var eyes = document.getElementById("userPSW");
-  eyes.type = "text";
+  inputPassword.type = 'text';
 }
 
 function hidePSW() {
-  var eyes = document.getElementById("userPSW");
-  eyes.type = "password";
+  inputPassword.type = 'password';
 }
 
-function ld() {
-  loading.classList.add("fa-circle-o-notch");
-  loading.classList.add("fa-spin");
-  setTimeout(() => {
-    loading.classList.remove("fa-circle-o-notch");
-    loading.classList.remove("fa-spin");
-  }, 3000);
-}
+// Export function with events.
+signupBtn.addEventListener('click', Signup);
+inputName.addEventListener('input', clearError);
+inputEmail.addEventListener('input', clearError);
+showBtn.addEventListener('click', showPSW);
+showBtn.addEventListener('mouseleave', hidePSW);
 
-sgp.addEventListener("click", Signup);
-uN.addEventListener("input", clearError);
-uE.addEventListener("input", clearError);
-showSNP.addEventListener("click", showPSW);
-showSNP.addEventListener("mouseleave", hidePSW);
-sgp.addEventListener("click", ld);
 if (module.hot) {
   module.hot.accept();
 }
