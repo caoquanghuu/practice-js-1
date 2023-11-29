073efb55d969a9ee23a040efe3bb06dd14f0d6bc
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2023-11-29 16:07:46 +0700

    fix: fix signup.js code

--- src/signup.js
+++ src/signup.js
@@ -1,82 +1,116 @@
-const signupBtn = document.getElementById('signup-button');
+const axios = require('axios').default;
+
+const btnSignUP = document.getElementById('btn-sign-up');
 const inputName = document.getElementById('input-name');
 const inputEmail = document.getElementById('input-email');
 const inputPhone = document.getElementById('input-phone');
 const inputPassword = document.getElementById('input-password');
-const loadingIcon = document.querySelector('.fa');
-const showBtn = document.getElementById('eye-signup');
-const showError = document.getElementById('exist-error');
-
-const axios = require('axios').default;
+const iconLoadingBtn = document.getElementById('icon-loading-btn');
+const btnShowPassword = document.getElementById('btn-show-password');
+const existError = document.getElementById('exist-error');
 
 function sleep(ms) {
   // eslint-disable-next-line no-promise-executor-return
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-async function Signup() {
+
+// Disable/Enable input field.
+function lockUI(lock) {
+  inputName.disabled = lock;
+  inputEmail.disabled = lock;
+  inputPhone.disabled = lock;
+  inputPassword.disabled = lock;
+}
+
+// Start/Stop loading button
+function loadingBtn(start) {
+  if (start) {
+    iconLoadingBtn.classList.add('fa-circle-o-notch');
+    iconLoadingBtn.classList.add('fa-spin');
+  } else {
+    iconLoadingBtn.classList.remove('fa-circle-o-notch');
+    iconLoadingBtn.classList.remove('fa-spin');
+  }
+}
+
+function togglePassword() {
+  const type = inputPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+  inputPassword.setAttribute('type', type);
+}
+
+// clear error.
+function clearError() {
+  existError.innerHTML = '';
+}
+
+function displayError(typeError) {
+  const missInfoError = 'Please insert all information!!!';
+  const existInfo = 'user / email already exist.';
+
+  if (typeError === 'missInfo') {
+    existError.innerHTML = missInfoError;
+  }
+  if (typeError === 'existInfo') {
+    existError.innerHTML = existInfo;
+  }
+}
+
+// Start sign up step
+async function signUp() {
   // Set variable inside
   const userName = inputName.value;
   const userEmail = inputEmail.value;
   const userPhone = inputPhone.value;
   const userPassword = inputPassword.value;
-  // Disable input field.
-  inputName.disabled = true;
-  inputEmail.disabled = true;
-  inputPhone.disabled = true;
-  inputPassword.disabled = true;
-  // Start loading button.
-  loadingIcon.classList.add('fa-circle-o-notch');
-  loadingIcon.classList.add('fa-spin');
+  // variables for check as boolean
+  let emptyInput;
+  let userExist;
 
+  // Lock UI
+  lockUI(true);
+
+  // Start loading button.
+  loadingBtn(true);
+
+  // Check all input field has been assigned or not
   if (!userName || !userEmail || !userPhone || !userPassword) {
-    showError.innerHTML = 'Please insert all information!!!';
-  } else {
+    displayError('missInfo');
+    emptyInput = true;
+  }
+  // Check user information exist on data server exist or not
+  if (!emptyInput) {
     const response = await axios.get('http://localhost:3000/user');
     await sleep(3000);
-    let checkUser;
-    for (let i = 0; i < response.data.length - 1; i += 1) {
-      if (userName === response.data[i].name || userEmail === response.data[i].email) {
-        checkUser = false;
-        showError.innerHTML = 'user / email already exist';
+    // eslint-disable-next-line array-callback-return
+    response.data.findIndex((data) => {
+      if (userName === data.name || userEmail === data.email) {
+        displayError('existInfo');
+        userExist = true;
       }
-    }
-    if (checkUser !== false) {
+    });
+    // Post user information to data server
+    if (!userExist) {
       axios.post('http://localhost:3000/user', {
         name: userName,
         email: userEmail,
-        userPSW: userPassword,
-        PhoneNumb: userPhone,
+        password: userPassword,
+        phoneNumb: userPhone,
       });
     }
   }
-  // Enable input field.
-  inputName.disabled = false;
-  inputEmail.disabled = false;
-  inputPhone.disabled = false;
-  inputPassword.disabled = false;
+
+  // Unlock UI
+  lockUI(false);
+
   // Stop loading button.
-  loadingIcon.classList.remove('fa-circle-o-notch');
-  loadingIcon.classList.remove('fa-spin');
-}
-// clear error.
-function clearError() {
-  document.getElementById('exist-error').innerHTML = '';
-}
-
-function showPSW() {
-  inputPassword.type = 'text';
-}
-
-function hidePSW() {
-  inputPassword.type = 'password';
+  loadingBtn(false);
 }
 
 // Export function with events.
-signupBtn.addEventListener('click', Signup);
+btnSignUP.addEventListener('click', signUp);
 inputName.addEventListener('input', clearError);
 inputEmail.addEventListener('input', clearError);
-showBtn.addEventListener('click', showPSW);
-showBtn.addEventListener('mouseleave', hidePSW);
+btnShowPassword.addEventListener('click', togglePassword);
 
 if (module.hot) {
   module.hot.accept();
