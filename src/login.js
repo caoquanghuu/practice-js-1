ae3db952370e85fd43a089a7a531561ed1ced0d1
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2023-12-04 18:17:10 +0700

    fix: js destructring and name variables

--- src/login.js
+++ src/login.js
@@ -1,87 +1,91 @@
-require('./axios-request')();
-require('./ultis')();
+window.addEventListener('load', () => {
+  require('./axios-request')();
+  require('./ultis')();
 
-// query element //
-const btnLogin = document.getElementById('btn-login');
-const btnShowPassword = document.getElementById('btn-show-password2');
-const iconLoadingBtn = document.getElementById('icon-loading-btn');
-const inputUser = document.getElementById('input-name');
-const inputPassword = document.getElementById('input-password');
-const existErrorText = document.getElementById('exist-error-text');
-const typeErrorDisplay = {
-  emptyInput: 'Please insert your User name / Email and password',
-  wrongUser: 'Wrong use name / password',
-};
+  // query element //
+  const btnLogin = document.getElementById('btn-login');
+  const btnShowPassword = document.getElementById('btn-show-password2');
+  const iconLoadingBtn = document.getElementById('icon-loading-btn');
+  const inputUser = document.getElementById('input-name');
+  const inputPassword = document.getElementById('input-password');
+  const existErrorText = document.getElementById('exist-error-text');
+  const [errorEmptyInput, errorWrongUser, clearError] = [
+    'Please insert your User name / Email and password',
+    'Wrong use name / password',
+    '',
+  ];
 
-function lockUI(lock) {
-  inputUser.disabled = lock;
-  inputPassword.disabled = lock;
-  if (lock) {
-    iconLoadingBtn.classList.add('fa-circle-o-notch');
-    iconLoadingBtn.classList.add('fa-spin');
-  } else {
-    iconLoadingBtn.classList.remove('fa-circle-o-notch');
-    iconLoadingBtn.classList.remove('fa-spin');
-  }
-}
-
-function setError(typeError) {
-  switch (typeError) {
-    case 'emptyInput':
-      existErrorText.innerHTML = typeErrorDisplay.emptyInput;
-      break;
-    case 'wrongUser':
-      existErrorText.innerHTML = typeErrorDisplay.wrongUser;
-      break;
-    case 'clearError':
-      existErrorText.innerHTML = '';
-      break;
-    default:
-  }
-}
-
-function togglePassword() {
-  const type = inputPassword.getAttribute('type') === 'password' ? 'text' : 'password';
-  inputPassword.setAttribute('type', type);
-}
-
-async function login() {
-  const userAccount = inputUser.value;
-  const userPassword = inputPassword.value;
-  let emptyInput = false;
-
-  lockUI(true);
-  if (!userAccount || !userPassword) {
-    setError('emptyInput');
-    emptyInput = true;
-  }
-  if (!emptyInput) {
-    const response = await getUserInformation;
-    await sleep(3000);
-    const userInfoInServer = response.data.some(
-      (data) =>
-        (data.name === userAccount || data.dataEmail === userAccount) &&
-        data.password === userPassword,
-    );
-    if (userInfoInServer) {
-      window.location.replace('calculator.html');
+  function lockUI(lock) {
+    inputUser.disabled = lock;
+    inputPassword.disabled = lock;
+    if (lock) {
+      iconLoadingBtn.classList.add('fa-circle-o-notch');
+      iconLoadingBtn.classList.add('fa-spin');
     } else {
-      setError('wrongUser');
+      iconLoadingBtn.classList.remove('fa-circle-o-notch');
+      iconLoadingBtn.classList.remove('fa-spin');
     }
   }
-  lockUI(false);
-}
 
-// button event
-btnLogin.addEventListener('click', login);
-btnShowPassword.addEventListener('click', togglePassword);
-inputUser.addEventListener('input', () => {
-  setError('clearError');
-});
-inputPassword.addEventListener('input', () => {
-  setError('clearError');
-});
+  function setError(typeError) {
+    switch (typeError) {
+      case errorEmptyInput:
+        existErrorText.innerHTML = errorEmptyInput;
+        break;
+      case errorWrongUser:
+        existErrorText.innerHTML = errorWrongUser;
+        break;
+      case clearError:
+        existErrorText.innerHTML = clearError;
+        break;
+      default:
+    }
+  }
 
-if (module.hot) {
-  module.hot.accept();
-}
+  function togglePassword() {
+    const type = inputPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+    inputPassword.setAttribute('type', type);
+  }
+
+  async function login() {
+    const userAccount = inputUser.value;
+    const userPassword = inputPassword.value;
+    let inputEmpty = false;
+
+    lockUI(true);
+    if (!userAccount || !userPassword) {
+      inputEmpty = true;
+    }
+    if (!inputEmpty) {
+      const response = await getUserInformation;
+      await sleep(3000);
+      const userInfoInServer = response.data.some(
+        (data) =>
+          (data.name === userAccount || data.dataEmail === userAccount) &&
+          data.password === userPassword,
+      );
+      if (userInfoInServer) {
+        window.location.replace('calculator.html');
+      } else {
+        setError(errorWrongUser);
+      }
+    }
+    if (inputEmpty) setError(errorEmptyInput);
+
+    lockUI(false);
+  }
+
+  // button event
+  btnLogin.addEventListener('mouseup', login);
+  btnShowPassword.addEventListener('mouseup', togglePassword);
+  inputUser.addEventListener('mouseup', () => {
+    setError(clearError);
+  });
+  inputPassword.addEventListener('mouseup', () => {
+    setError(clearError);
+  });
+
+  if (module.hot) {
+    module.hot.accept();
+  }
+});
--- src/signup.js
+++ src/signup.js
@@ -1,99 +1,102 @@
-require('./ultis')();
-// Export getUserInformation and function postUserInformation.
-require('./axios-request');
-require('./axios-request')();
+window.addEventListener('load', () => {
+  require('./ultis')();
+  // Export getUserInformation and function postUserInformation.
+  require('./axios-request');
+  require('./axios-request')();
 
-const btnSignUP = document.getElementById('btn-sign-up');
-const inputName = document.getElementById('input-name');
-const inputEmail = document.getElementById('input-email');
-const inputPhone = document.getElementById('input-phone');
-const inputPassword = document.getElementById('input-password');
-const iconLoadingBtn = document.getElementById('icon-loading-btn');
-const btnShowPassword = document.getElementById('btn-show-password');
-const existErrorText = document.getElementById('exist-error-text');
-const typeErrorDisplay = {
-  missInfo: 'Please insert all information!!!',
-  existInfo: 'user / email already exist!!!',
-};
+  const btnSignUP = document.getElementById('btn-sign-up');
+  const inputName = document.getElementById('input-name');
+  const inputEmail = document.getElementById('input-email');
+  const inputPhone = document.getElementById('input-phone');
+  const inputPassword = document.getElementById('input-password');
+  const iconLoadingBtn = document.getElementById('icon-loading-btn');
+  const btnShowPassword = document.getElementById('btn-show-password');
+  const spaceDisplayError = document.getElementById('space-display-error');
+  const [errorEmptyInput, errorExistInfo, clearError] = [
+    'Please insert all information!!!',
+    'user / email already exist!!!',
+    '',
+  ];
 
-// Disable/Enable input field.
-function lockUI(lock) {
-  inputName.disabled = lock;
-  inputEmail.disabled = lock;
-  inputPhone.disabled = lock;
-  inputPassword.disabled = lock;
-  if (lock) {
-    iconLoadingBtn.classList.add('fa-circle-o-notch');
-    iconLoadingBtn.classList.add('fa-spin');
-  } else {
-    iconLoadingBtn.classList.remove('fa-circle-o-notch');
-    iconLoadingBtn.classList.remove('fa-spin');
-  }
-}
-
-function togglePassword() {
-  const type = inputPassword.getAttribute('type') === 'password' ? 'text' : 'password';
-  inputPassword.setAttribute('type', type);
-}
-
-function setError(typeError) {
-  switch (typeError) {
-    case 'missInfo':
-      existErrorText.innerHTML = typeErrorDisplay.missInfo;
-      break;
-    case 'existInfo':
-      existErrorText.innerHTML = typeErrorDisplay.existInfo;
-      break;
-    case 'clearError':
-      existErrorText.innerHTML = '';
-      break;
-    default:
-  }
-}
-
-// Start sign up step
-async function signUp() {
-  // Set variable inside
-  const userName = inputName.value;
-  const userEmail = inputEmail.value;
-  const userPhone = inputPhone.value;
-  const userPassword = inputPassword.value;
-  // variables for check as boolean
-  let emptyInput = false;
-
-  // Lock UI
-  lockUI(true);
-  // Check all input field has been assigned or not
-  if (!userName || !userEmail || !userPhone || !userPassword) {
-    setError('missInfo');
-    emptyInput = true;
-  }
-  // Check user information exist on data server exist or not
-  if (!emptyInput) {
-    const response = await getUserInformation;
-    await sleep(3000);
-    const dataName = response.data.findIndex((data) => data.name === userName);
-    const dataEmail = response.data.findIndex((data) => data.email === userEmail);
-    if (dataName !== -1 || dataEmail !== -1) {
-      setError('existInfo');
+  // Disable/Enable input field.
+  function lockUI(lock) {
+    inputName.disabled = lock;
+    inputEmail.disabled = lock;
+    inputPhone.disabled = lock;
+    inputPassword.disabled = lock;
+    if (lock) {
+      iconLoadingBtn.classList.add('fa-circle-o-notch');
+      iconLoadingBtn.classList.add('fa-spin');
     } else {
-      await postUserInformation(userName, userEmail, userPassword, userPhone);
+      iconLoadingBtn.classList.remove('fa-circle-o-notch');
+      iconLoadingBtn.classList.remove('fa-spin');
     }
   }
-  // Unlock UI
-  lockUI(false);
-}
 
-// Export function with events.
-btnSignUP.addEventListener('click', signUp);
-inputName.addEventListener('click', () => {
-  setError('clearError');
-});
-inputEmail.addEventListener('click', () => {
-  setError('clearError');
-});
-btnShowPassword.addEventListener('click', togglePassword);
+  function togglePassword() {
+    const type = inputPassword.getAttribute('type') === 'password' ? 'text' : 'password';
+    inputPassword.setAttribute('type', type);
+  }
 
-if (module.hot) {
-  module.hot.accept();
-}
+  function displayError(typeError) {
+    switch (typeError) {
+      case errorEmptyInput:
+        spaceDisplayError.innerHTML = errorEmptyInput;
+        break;
+      case errorExistInfo:
+        spaceDisplayError.innerHTML = errorExistInfo;
+        break;
+      case clearError:
+        spaceDisplayError.innerHTML = clearError;
+        break;
+      default:
+    }
+  }
+
+  // Start sign up step
+  async function signUp() {
+    // Set variable inside
+    const userName = inputName.value;
+    const userEmail = inputEmail.value;
+    const userPhone = inputPhone.value;
+    const userPassword = inputPassword.value;
+    // variables for check as boolean
+    let inputEmpty = false;
+
+    // Lock UI
+    lockUI(true);
+    // Check all input field has been assigned or not
+    if (!userName || !userEmail || !userPhone || !userPassword) {
+      inputEmpty = true;
+    }
+    // Check user information exist on data server exist or not
+    if (!inputEmpty) {
+      const response = await getUserInformation;
+      await sleep(3000);
+      const dataName = response.data.findIndex((data) => data.name === userName);
+      const dataEmail = response.data.findIndex((data) => data.email === userEmail);
+      if (dataName !== -1 || dataEmail !== -1) {
+        displayError(errorExistInfo);
+      } else {
+        await postUserInformation(userName, userEmail, userPassword, userPhone);
+      }
+    }
+    if (inputEmpty) displayError(errorEmptyInput);
+    // Unlock UI
+    lockUI(false);
+  }
+
+  // Export function with events.
+  btnSignUP.addEventListener('mouseup', signUp);
+  inputName.addEventListener('mouseup', () => {
+    displayError(clearError);
+  });
+  inputEmail.addEventListener('mouseup', () => {
+    displayError(clearError);
+  });
+  btnShowPassword.addEventListener('mouseup', togglePassword);
+
+  if (module.hot) {
+    module.hot.accept();
+  }
+});
