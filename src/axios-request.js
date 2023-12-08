5790c28721083d7d5423b6d14bb05f76b3d0529d
caoquanghuu <147122360+caoquanghuu@users.noreply.github.com>
2023-12-08 17:42:16 +0700

    fix: change get request to be a function.

--- src/axios-request.js
+++ src/axios-request.js
@@ -1,18 +1,20 @@
 const axios = require('axios').default;
 
 module.exports = function () {
-  this.getUserInformation = axios.get('http://localhost:3000/user').catch((error) => {
-    if (error.response) {
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      console.log(error.request);
-    } else {
-      console.log('Error', error.message);
-    }
-    console.log(error.config);
-  });
+  this.getUserInformation = function () {
+    return axios.get('http://localhost:3000/user').catch((error) => {
+      if (error.response) {
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      } else if (error.request) {
+        console.log(error.request);
+      } else {
+        console.log('Error', error.message);
+      }
+      console.log(error.config);
+    });
+  };
   this.postUserInformation = function (userName, userEmail, userPassword, userPhone) {
     axios
       .post('http://localhost:3000/user', {
